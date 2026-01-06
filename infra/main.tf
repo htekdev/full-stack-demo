01aed5f1cdb108fe4ec1c838f68e4e3b98dff9a4
Hector Flores <hector.flores@htek.dev>
2026-01-06 11:26:32 -0600

    Change Static Web App to Standard SKU for function app registration

--- infra/main.tf
+++ infra/main.tf
@@ -109,8 +109,8 @@ resource "azurerm_static_web_app" "main" {
   name                = local.static_web_app_name
   resource_group_name = azurerm_resource_group.main.name
   location            = "westus2"  # SWA limited regions
-  sku_tier            = "Free"
-  sku_size            = "Free"
+  sku_tier            = "Standard"
+  sku_size            = "Standard"
 
   tags = local.common_tags
 }
