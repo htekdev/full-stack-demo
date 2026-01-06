facc2c47bf2e42092b0e98485311eb5b8b7b7454
Hector Flores <hector.flores@htek.dev>
2026-01-06 11:54:28 -0600

    Add create_before_destroy lifecycle to Function App

--- infra/main.tf
+++ infra/main.tf
@@ -88,6 +88,7 @@ resource "azurerm_linux_function_app" "main" {
   tags = local.common_tags
 
   lifecycle {
+    create_before_destroy = true
     ignore_changes = [
       app_settings["WEBSITE_RUN_FROM_PACKAGE"],
     ]
